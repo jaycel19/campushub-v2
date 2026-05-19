d076866b0d251f824c6c4a0b7aacef685202684a
Jhun Chester Lalongisip <jhunchesterlalongisip@gmail.com>
2026-05-19 10:19:00 +0800

    refactor(auth): set user_id from JWT claims

--- backend/pkg/middlewares/auth.go
+++ backend/pkg/middlewares/auth.go
@@ -28,6 +28,16 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid claims",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("user_id", claims["user_id"])
 		c.Next()
 	}
 }
